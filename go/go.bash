eddb0b446478551ef29336902104f9fd217ac42b
John Asmuth <jasmuth@google.com>
2017-01-18 15:45:00 -0500

    Create two versions of the go tool builder, one with an golang:alpine base and one with a golang:wheezy base.
    Point gcr.io/cloud-builders/go:latest to gcr.io/cloud-builders/go:alpine.
    
    This update allows people to easily build go binaries for the environment of their
    choice without requiring CGO_ENABLED=0. Disabling cgo means no dynamic linking,
    which allows fun things like sqlite.
    
    -------------
    MOE_MIGRATED_REVID=144868739
    APPROVED=

---
+++ go/go.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright 2016 Google, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+. /builder/prepare_workspace.inc
+prepare_workspace || exit
+echo "Documentation at https://github.com/GoogleCloudPlatform/cloud-builders/blob/master/go/README.md"
+if [[ "$1" == install ]]; then
+  # Give a hint about where binaries end up if we think they're using 'go install'.
+  gp=$(go env GOPATH)
+  binpath=${gp#$PWD/}/bin
+  echo "Binaries built using 'go install' will go to \"$binpath\"."
+fi
+echo "Running: go $@"
+go "$@"
