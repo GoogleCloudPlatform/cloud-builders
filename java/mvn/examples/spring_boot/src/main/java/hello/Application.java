57bfdc4358298b8689c929feedb2241d334fbb84
John Asmuth <jasmuth@google.com>
2017-05-18 14:58:41 -0400

    Addressed some lint complaints.

--- java/mvn/examples/spring_boot/src/main/java/hello/Application.java
+++ java/mvn/examples/spring_boot/src/main/java/hello/Application.java
@@ -3,6 +3,7 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Application example. */
 @SpringBootApplication
 public class Application {
 
--- java/mvn/examples/spring_boot/src/main/java/hello/GreetingController.java
+++ java/mvn/examples/spring_boot/src/main/java/hello/GreetingController.java
@@ -5,15 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/** GreetingController example. */
 @RestController
 public class GreetingController {
 
-  private static final String template = "Hello, %s! You are visitor number %s";
+  private static final String TEMPLATE = "Hello, %s! You are visitor number %s";
   private final AtomicLong counter = new AtomicLong();
 
   @RequestMapping("/")
-  public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
-    return String.format(template, name, counter.incrementAndGet());
+  public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format(TEMPLATE, name, counter.incrementAndGet());
   }
 
 }
