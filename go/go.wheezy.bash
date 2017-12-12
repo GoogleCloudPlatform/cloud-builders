c8556ad65ef73624de371ed1e5747b5d82256e75
Jason Hall <jasonhall@google.com>
2017-12-12 10:26:30 -0500

    Break the go:wheezy builder
    It's been deprecated with a warning for 3 months.
    
    After this is released, we'll stop building new go:wheezy images.

--- go/go.wheezy.bash
+++ go/go.wheezy.bash
@@ -19,17 +19,8 @@ prepare_workspace || exit
 
 cat << EOF
 *******************************************************************************
-*** NOTICE: The 'go:wheezy' builder is deprecated. Use 'go:debian' instead. ***
-*** Starting no earlier than December 12, 2017, this builder will be broken ***
-*** and will no longer work.                                                ***
+*** ERROR: The 'go:wheezy' builder is deprecated. Use 'go:debian' instead.  ***
 *******************************************************************************
 EOF
 
-if [[ "$1" == install ]]; then
-  # Give a hint about where binaries end up if we think they're using 'go install'.
-  gp=$(go env GOPATH)
-  binpath=${gp#$PWD/}/bin
-  echo "Binaries built using 'go install' will go to \"$binpath\"."
-fi
-echo "Running: go $@"
-go "$@"
+exit 1
