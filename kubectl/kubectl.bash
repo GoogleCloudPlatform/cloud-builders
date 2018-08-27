7ad9600400a6c9202273d7de0e3187490b38ae88
mickybart <mickybart@pygoscelis.org>
2018-08-27 10:27:32 -0400

    kubectl: support regional GKE clusters
    from: https://github.com/GoogleCloudPlatform/cloud-builders-community/commit/2e41b4885430f5755dc311c8ce0194a2ff94ba9f#diff-97526577b471dfab62ae6fb4614b2518

--- kubectl/kubectl.bash
+++ kubectl/kubectl.bash
@@ -2,12 +2,14 @@
 
 # always get a new context
 cluster=$(gcloud config get-value container/cluster 2> /dev/null)
+region=${CLOUDSDK_COMPUTE_REGION:-$(gcloud config get-value compute/region 2> /dev/null)}
 zone=$(gcloud config get-value compute/zone 2> /dev/null)
 project=$(gcloud config get-value core/project 2> /dev/null)
 
 function var_usage() {
     cat <<EOF
-No cluster is set. To set the cluster (and the zone where it is found), set the environment variables
+No cluster is set. To set the cluster (and the region/zone where it is found), set the environment variables
+CLOUDSDK_COMPUTE_REGION=<cluster region> (regional clusters)
 CLOUDSDK_COMPUTE_ZONE=<cluster zone>
 CLOUDSDK_CONTAINER_CLUSTER=<cluster name>
 EOF
@@ -15,10 +17,17 @@ EOF
 }
 
 [[ -z "$cluster" ]] && var_usage
-[[ -z "$zone" ]] && var_usage
+[ ! "$zone" -o "$region" ] && var_usage
 
-echo "Running: gcloud container clusters get-credentials --project=\"$project\" --zone=\"$zone\" \"$cluster\""
-gcloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster" || exit
+if [ -n "$region" ]; then
+  echo "Running: gcloud config set container/use_v1_api_client false"
+  gcloud config set container/use_v1_api_client false
+  echo "Running: gcloud beta container clusters get-credentials --project=\"$project\" --region=\"$region\" \"$cluster\""
+  gcloud beta container clusters get-credentials --project="$project" --region="$region" "$cluster" || exit
+else
+  echo "Running: gcloud container clusters get-credentials --project=\"$project\" --zone=\"$zone\" \"$cluster\""
+  gcloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster" || exit
+fi
 
 echo "Running: kubectl $@"
 kubectl "$@"
