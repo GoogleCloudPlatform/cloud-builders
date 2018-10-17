5a59169564d857f2a8de7e3dfacb8e301302ed06
Dave Protasowski <dprotaso@gmail.com>
2018-10-17 12:42:42 -0400

    gcs fetcher and uploader containers now contain third party licenses (#389)
    * include third-party licenses in the gcs-* containers
    
    * turn off go modules for dep-collector to work succesfully

--- gcs-fetcher/update-deps.sh
+++ gcs-fetcher/update-deps.sh
@@ -24,5 +24,16 @@ go get -u
 # Fetch dependencies into vendor/
 go mod vendor
 
+# These commands need to be in your $PATH
+# github.com/mattmoor/dep-collector
+# github.com/google/licenseclassifier
+
+dep-collector -check ./cmd/gcs-fetcher
+dep-collector -check ./cmd/gcs-uploader
+
+# These require go modules to be turned off to be successfull
+GO111MODULE=off dep-collector ./cmd/gcs-fetcher > ./cmd/gcs-fetcher/VENDOR-LICENSE
+GO111MODULE=off dep-collector ./cmd/gcs-uploader > ./cmd/gcs-uploader/VENDOR-LICENSE
+
 # Remove tests in vendor/
 rm -rf $(find vendor/ -name '*_test.go')
