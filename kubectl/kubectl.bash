ad9d202b63c0a30fe4b88fbe4f7b461158abc5fd
Jason Hall <jasonhall@google.com>
2019-07-23 10:22:25 -0400

    Always get GKE credentials

--- kubectl/kubectl.bash
+++ kubectl/kubectl.bash
@@ -14,23 +14,21 @@ EOF
 # If there is no kubectl context, use gcloud to get one, otherwise we can
 # assume the user hase configured the context with some kubectl related env
 # var.
-if [[ $(kubectl config current-context 2> /dev/null) == "" ]]; then
-  cluster=$(gcloud config get-value container/cluster 2> /dev/null)
-  region=${CLOUDSDK_COMPUTE_REGION:-$(gcloud config get-value compute/region 2> /dev/null)}
-  zone=$(gcloud config get-value compute/zone 2> /dev/null)
-  project=$(gcloud config get-value core/project 2> /dev/null)
+cluster=$(gcloud config get-value container/cluster 2> /dev/null)
+region=${CLOUDSDK_COMPUTE_REGION:-$(gcloud config get-value compute/region 2> /dev/null)}
+zone=$(gcloud config get-value compute/zone 2> /dev/null)
+project=$(gcloud config get-value core/project 2> /dev/null)
 
-  [[ -z "$cluster" ]] && var_usage
-  [ ! "$zone" -o "$region" ] && var_usage
+[[ -z "$cluster" ]] && var_usage
+[ ! "$zone" -o "$region" ] && var_usage
 
-  if [ -n "$region" ]; then
-    echo "Running: gcloud container clusters get-credentials --project=\"$project\" --region=\"$region\" \"$cluster\""
-    gcloud container clusters get-credentials --project="$project" --region="$region" "$cluster" || exit
-  else
-    echo "Running: gcloud container clusters get-credentials --project=\"$project\" --zone=\"$zone\" \"$cluster\""
-    gcloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster" || exit
-  fi
-fi
+if [ -n "$region" ]; then
+  echo "Running: gcloud container clusters get-credentials --project=\"$project\" --region=\"$region\" \"$cluster\""
+  gcloud container clusters get-credentials --project="$project" --region="$region" "$cluster" || exit
+else
+  echo "Running: gcloud container clusters get-credentials --project=\"$project\" --zone=\"$zone\" \"$cluster\""
+  gcloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster" || exit
+ fi
 
 echo "Running: kubectl $@" >&2
 exec kubectl "$@"
