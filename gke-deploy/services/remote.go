2cf24b6e97dbc05cd717221eb64d239c2b918ad8
Joon Lim <joonlim@google.com>
2020-01-29 13:46:42 -0500

    Remove redundant "v1".

--- gke-deploy/services/remote.go
+++ gke-deploy/services/remote.go
@@ -17,7 +17,7 @@ import (
 
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
-	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 )
 
