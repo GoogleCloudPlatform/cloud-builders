643e05f354da5b84a5cbffc6dd391473cf69ba25
udatny <daniel.udatny@gmail.com>
2020-04-19 15:28:48 +0200

    Update kubectl.bash
    get rid of gcloud deps
    
    improving change to become compatible to original
    
    fixes
    
    Update kubectl.bash
    
    Update kubectl.bash
    
    Update kubectl.bash
    
    Update kubectl.bash
    
    Update kubectl.bash

--- kubectl/kubectl.bash
+++ kubectl/kubectl.bash
@@ -8,7 +8,7 @@ function var_usage() {
   CLOUDSDK_COMPUTE_REGION=<cluster region> (regional clusters)
   CLOUDSDK_COMPUTE_ZONE=<cluster zone>
   CLOUDSDK_CONTAINER_CLUSTER=<cluster name>
-
+  KUBERNETES_CONFIG_FILE=path to alternate kubectl config in your workspace
   Optionally, you can specify the kubectl version via KUBECTL_VERSION, taking one of the following values: ${versions[@]}
 EOF
 
@@ -16,25 +16,34 @@ EOF
 }
 
 kubectl_cmd=kubectl${KUBECTL_VERSION:+.${KUBECTL_VERSION}}
-cluster=${CLOUDSDK_CONTAINER_CLUSTER:-$(gcloud config get-value container/cluster 2> /dev/null)}
-region=${CLOUDSDK_COMPUTE_REGION:-$(gcloud config get-value compute/region 2> /dev/null)}
-zone=${CLOUDSDK_COMPUTE_ZONE:-$(gcloud config get-value compute/zone 2> /dev/null)}
-project=${CLOUDSDK_CORE_PROJECT:-$(gcloud config get-value core/project 2> /dev/null)}
 
-[[ -z "$cluster" ]] && var_usage
-[ ! "$zone" -o "$region" ] && var_usage
-if [[ -n "$KUBECTL_VERSION" ]] && [[ ! " ${versions[*]} "  =~ " ${KUBECTL_VERSION} "  ]]; then
-  echo "Bad KUBECTL_VERSION \"${KUBECTL_VERSION}\". Expected one of ${versions[*]}" >&2
-  exit 1
+if [ -z "$KUBERNETES_CONFIG_FILE" ]; then
+  cluster=${CLOUDSDK_CONTAINER_CLUSTER:-$(gcloud config get-value container/cluster 2> /dev/null)}
+  region=${CLOUDSDK_COMPUTE_REGION:-$(gcloud config get-value compute/region 2> /dev/null)}
+  zone=${CLOUDSDK_COMPUTE_ZONE:-$(gcloud config get-value compute/zone 2> /dev/null)}
+  project=${CLOUDSDK_CORE_PROJECT:-$(gcloud config get-value core/project 2> /dev/null)}
+
+  [[ -z "$cluster" ]] && var_usage
+  [ ! "$zone" -o "$region" ] && var_usage
+  if [[ -n "$KUBECTL_VERSION" ]] && [[ ! " ${versions[*]} "  =~ " ${KUBECTL_VERSION} "  ]]; then
+    echo "Bad KUBECTL_VERSION \"${KUBECTL_VERSION}\". Expected one of ${versions[*]}" >&2
+    exit 1
+  fi
+
+  if [ -n "$region" ]; then
+    echo "Running: gcloud container clusters get-credentials --project=\"$project\" --region=\"$region\" \"$cluster\""
+    gcloud container clusters get-credentials --project="$project" --region="$region" "$cluster" || exit
+  else
+    echo "Running: gcloud container clusters get-credentials --project=\"$project\" --zone=\"$zone\" \"$cluster\""
+    gcloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster" || exit
+   fi
 fi
 
-if [ -n "$region" ]; then
-  echo "Running: gcloud container clusters get-credentials --project=\"$project\" --region=\"$region\" \"$cluster\""
-  gcloud container clusters get-credentials --project="$project" --region="$region" "$cluster" || exit
-else
-  echo "Running: gcloud container clusters get-credentials --project=\"$project\" --zone=\"$zone\" \"$cluster\""
-  gcloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster" || exit
- fi
-
 echo "Running: ${kubectl_cmd}" "$@" >&2
-exec "${kubectl_cmd}" "$@"
+
+
+if [ -z "$KUBERNETES_CONFIG_FILE" ]; then
+  exec "${kubectl_cmd}" "$@"
+else
+  exec "${kubectl_cmd}" "--kubeconfig=${KUBERNETES_CONFIG_FILE}" "$@"
+fi
