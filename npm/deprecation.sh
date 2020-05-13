e03dbc8aceb190e0636f332aded6d337a8ab6f02
David Bendory <bendory@google.com>
2020-05-13 10:09:35 -0400

    Added deprecation notices.

---
+++ npm/deprecation.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo           \*\*\*\*\* DEPRECATION NOTICE \*\*\*\*\*
+echo
+echo This image is deprecated and will no longer be updated.
+echo This recent version of the image will continue to exist.
+echo
+echo In place of this image, please use one of the official
+echo \`node\` images maintained by the Node.js Docker Team.
+echo For details, visit https://hub.docker.com/_/node.
+echo
+echo           \*\*\*\*\* DEPRECATION NOTICE \*\*\*\*\*
+
+npm $@
