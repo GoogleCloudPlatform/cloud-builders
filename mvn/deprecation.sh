a43e6c785a6b908f35ec882037f3ca27894d83d8
David Bendory <bendory@google.com>
2020-05-14 12:24:14 -0400

    Added deprecation notice for mvn image.

---
+++ mvn/deprecation.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo
+echo           \*\*\*\*\* DEPRECATION NOTICE \*\*\*\*\*
+echo
+echo This image is deprecated and will no longer be updated.
+echo This recent version of the image will continue to exist.
+echo
+echo In place of this image, please use one of the official
+echo \`maven\` images.
+echo For details, visit https://hub.docker.com/_/maven.
+echo
+echo           \*\*\*\*\* DEPRECATION NOTICE \*\*\*\*\*
+echo
+mvn $@
