b861fce421a293fed3619ed64a470d6ef33814d5
David Bendory <bendory@users.noreply.github.com>
2020-05-15 11:56:13 -0400

    Deprecate javac builder. (#668)
    * Added deprecation notice for mvn image.
    
    * Added deprecation notice for Gradle builder.
    
    * Fixed broken cloudbuild.yaml to pull javac image from cloud-builders repo.
    
    * Deprecated mvn and gradle images.
    
    * Deprecated javac builder.

---
+++ javac/deprecation.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo
+echo           \*\*\*\*\* DEPRECATION NOTICE \*\*\*\*\*
+echo
+echo This image is deprecated and will no longer be updated.
+echo This recent version of the image will continue to exist.
+echo
+echo In place of this image, please use one of the official
+echo \`openjdk\` images maintained by the Docker Community.
+echo For details, visit https://hub.docker.com/_/openjdk.
+echo
+echo           \*\*\*\*\* DEPRECATION NOTICE \*\*\*\*\*
+echo
+javac $@
