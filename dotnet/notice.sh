62b99494da191d883884a9d58d9803c61b2312f3
David Bendory <bendory@google.com>
2020-05-20 14:31:48 -0400

    Added alternative official dotnet image.

---
+++ dotnet/notice.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo '
+                   ***** NOTICE *****
+
+An official `microsoft/dotnet:sdk` image to run the `dotnet` tool exists at
+https://hub.docker.com/r/microsoft/dotnet.
+
+                   ***** NOTICE *****
+'
+dotnet $@
