a7d2b5ea3d64ffa36c1720f5a69ee302bd0b375b
David Bendory <bendory@google.com>
2020-05-20 14:14:14 -0400

    Added alternative images notice for curl.

---
+++ curl/notice.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo '
+                   ***** NOTICE *****
+
+Supported `curl` images, including multiple tagged versions,
+are available at
+https://console.cloud.google.com/launcher/details/google/ubuntu1604
+and https://hub.docker.com/r/curlimages/curl.
+
+                   ***** NOTICE *****
+'
+curl $@
