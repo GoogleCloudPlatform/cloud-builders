5e51099c91f3fbddebcdc322cf9ac56861d7b703
David Bendory <bendory@google.com>
2020-06-03 06:59:30 -0400

    Added notice pointing to additional git information.

---
+++ git/notice.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo '
+                   ***** NOTICE *****
+
+Official `cloud-sdk` images, including multiple tagged versions across multiple
+platforms, can be found at
+https://github.com/GoogleCloudPlatform/cloud-sdk-docker and may be more suitable
+for some use cases when interacting with Cloud Source Repositories.
+
+For additional information, please visit
+https://github.com/GoogleCloudPlatform/cloud-builders/tree/master/git
+
+                ***** END OF NOTICE *****
+'
+
+git "$@"
