385d163eda0094eae79065714b795a68451e163a
Appu Goundan <appu@google.com>
2020-10-05 14:02:08 -0400

    Add appengine example for testing

---
+++ mvn/examples/hello_appengine/src/main/java/com/example/MyServlet.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("Please use the form to POST to this url");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        String name = req.getParameter("name");
+        resp.setContentType("text/plain");
+        if(name == null) {
+            resp.getWriter().println("Please enter a shame");
+        }
+        resp.getWriter().println("Hello " + name);
+    }
+}
