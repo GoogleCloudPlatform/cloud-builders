84221f042c54c9decd06f1eaec38909c7d528dc3
David Bendory <bendory@google.com>
2020-10-06 10:07:37 -0400

    Explicitly import instead of '*'.

--- mvn/examples/hello_appengine/src/main/java/com/example/MyServlet.java
+++ mvn/examples/hello_appengine/src/main/java/com/example/MyServlet.java
@@ -1,7 +1,9 @@
 package com.example;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MyServlet extends HttpServlet {
     @Override
