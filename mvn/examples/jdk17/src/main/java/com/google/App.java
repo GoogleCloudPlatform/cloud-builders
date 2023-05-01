a2b5aef4435dbd152aeb8081ecc6942c5218f8a5
mwiczer <wiczer@google.com>
2023-05-01 12:31:49 -0400

    Formatting fix from #909 (#911)

--- mvn/examples/jdk17/src/main/java/com/google/App.java
+++ mvn/examples/jdk17/src/main/java/com/google/App.java
@@ -3,16 +3,12 @@ package com.google;
 /**
  * Hello world using the Record feature of jdk17 to ensure that the builder we are using supports at
  * least jdk17.
- *
  */
+public class App {
+  record Person(String firstName, String lastName) {}
 
-public class App
-{
-    record Person(String firstName, String lastName) { }
-
-    public static void main( String[] args )
-    {
-      Person person = new Person("John", "Doe");
-      System.out.println( "Hello " + person.firstName + " " + person.lastName + "!" );
-    }
+  public static void main(String[] args) {
+    Person person = new Person("John", "Doe");
+    System.out.println("Hello " + person.firstName + " " + person.lastName + "!");
+  }
 }
